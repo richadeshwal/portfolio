c9fd7a61230bd51280152b4763de5876695dfa1a
meghna.khound@hpe.com <deshwalricha8@gmail.com>
2018-09-23 09:20:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LeisureComponent } from './leisure/leisure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    HomeComponent,
+    ContactComponent,
+    LeisureComponent
   ],
   imports: [
     BrowserModule,
